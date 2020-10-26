de65b5a5f761163f3d42348e0237956fc398dd8d
greg-el <gr3gl@hotmail.com>
2020-10-26 04:15:33 +0000

    add preceding zero to hour when hour less than 10

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -4,11 +4,12 @@ class Clock extends React.Component {
   render() {
     let d = new Date();
     let hour = d.getHours();
+    let fHour = (hour < 10 ? "0" : "") + hour;
     let minute = d.getMinutes();
     let fMiniute = (minute < 10 ? "0" : "") + minute;
     return (
       <div id="clock-wrapper">
-        <div id="clock">{hour + ":" + fMiniute}</div>
+        <div id="clock">{fHour + ":" + fMiniute}</div>
       </div>
     );
   }
