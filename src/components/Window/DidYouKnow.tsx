87d868f53d76ac2df407bc0d544def1180daf79d
greg-el <gr3gl@hotmail.com>
2020-10-26 03:55:17 +0000

    add line to did-you-knows

--- src/components/Window/DidYouKnow.tsx
+++ src/components/Window/DidYouKnow.tsx
@@ -1,28 +1,47 @@
 import React from "react";
 
-function randomDidYouKnow() {
-  let didYouKnows = ["You should hire me as a junior dev"];
-
-  return didYouKnows[Math.floor(Math.random() * didYouKnows.length)];
+interface IState {
+  didYouKnowText: string;
 }
 
-function DidYouKnow() {
-  return (
-    <div id="dyk-container">
-      <div id="dyk-icon-wrapper">
-        <div id="dyk-icon"></div>
-      </div>
-      <div id="dyk-main-wrapper">
-        <div id="dyk-text-wrapper">
-          <div id="dyk-did-you-know-message">Did you know...</div>
-          <div id="dyk-text">{randomDidYouKnow()}</div>
+interface IProps {}
+
+class DidYouKnow extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      didYouKnowText: this.randomDidYouKnow(),
+    };
+  }
+
+  randomDidYouKnow() {
+    let didYouKnows = [
+      "You should hire me as a dev",
+      "To open a program, you just click the Start button, and then click the program's icon",
+    ];
+
+    return didYouKnows[Math.floor(Math.random() * didYouKnows.length)];
+  }
+
+  render() {
+    return (
+      <div id="dyk-container">
+        <div id="dyk-icon-wrapper">
+          <div id="dyk-icon"></div>
         </div>
-        <div id="dyk-pc-wrapper">
-          <div id="dyk-pc"></div>
+        <div id="dyk-main-wrapper">
+          <div id="dyk-text-wrapper">
+            <div id="dyk-did-you-know-message">Did you know...</div>
+            <div id="dyk-text">{this.state.didYouKnowText}</div>
+          </div>
+          <div id="dyk-pc-wrapper">
+            <div id="dyk-pc"></div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default DidYouKnow;
